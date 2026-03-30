4c72cbc40162db0ecd24b900a5139e48c3cc96fc
Vincent Huisman <info@impresseasy.com>
2026-03-30 14:24:39 +0200

    1.2

--- src/app/components/home-container/home-container.ts
+++ src/app/components/home-container/home-container.ts
@@ -2,10 +2,11 @@ import {Component} from '@angular/core';
 import {Homepage} from '../homepage/homepage';
 import {Projecten} from '../projecten/projecten';
 import {OverMij} from '../over-mij/over-mij';
+import {Contact} from '../contact/contact';
 
 @Component({
   selector: 'app-home-container',
-  imports: [Homepage, Projecten, OverMij],
+  imports: [Homepage, Projecten, OverMij, Contact],
   templateUrl: './home-container.html',
   styleUrl: './home-container.css',
 })
